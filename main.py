83238df28785e79724841dd1ee4d1c6a7c6448b2
Paulina Palczynska <paulinepalczynska@gmail.com>
2023-07-01 11:58:27 +0200

    Fix is_prime

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ def is_prime(n):
 	if n == 2:
 		return True
 
-	for i in range(2, n):
-		if n%2 ==0:
+	if n % 2 == 0:
+		return False
+
+	for i in range(3, int(n ** 0.5)+1,2):
+		if n%i ==0:
 			return False
 	return True
 
 for i in range(20):
 	print(i, is_prime(i))
-
