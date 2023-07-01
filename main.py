aff4558cc4014376252ea1593419d81c8c5cf04a
Paulina Palczynska <paulinepalczynska@gmail.com>
2023-07-01 11:40:52 +0200

    Add fibonacci function

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ def is_prime(n):
 for i in range(20):
 	print(i, is_prime(i))
 
+
+
+def fibo(n):
+	if n in (0, 1):
+		return n
+
+	n1, n2 = 1, 0
+	for i in range(n-1):
+		n1, n2 = n1 + n2, n1
+
+	return n1
+
+print([fibo(i) for i in range(10)])
